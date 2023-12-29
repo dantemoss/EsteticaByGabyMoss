fc1afdef29ab1bcb127cdff2364390941062f82a
Dante Moss <dantemoss3@gmail.com>
2023-12-29 19:56:47 -0300

    services section created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
 import TextWelcome from "@/components/WelcomeBanner/TextWelcome.jsx";
 import Navbar  from "../components/Navbar/Navbar.jsx";
+import Service from "@/components/OurServices/Services.jsx";
+import Sponsors from "@/components/Sponsors/Sponsors.jsx";
 
 
 export default function Page() {
   return (
     <div className="w-screen h-screen">
       <Navbar/>
-      <TextWelcome/> 
+      <TextWelcome/>
+      <Service/>
+      <Sponsors/>
       </div>
   );
 }
